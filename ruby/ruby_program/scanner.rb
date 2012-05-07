cf582544d9a2dde760419a58a96711bc2c2d8589
wuxiang <wuxiangfe@qq.com>
2012-05-06 22:58:09 -0700

    scanner.rb

--- ruby/ruby_program/scanner.rb
+++ ruby/ruby_program/scanner.rb
@@ -3,27 +3,36 @@ require 'rubygems'
 require 'net/ssh'
 require 'net/scp'
 
-Text_endcolor = "\033[0m";
-Text_red      = "\033[22;31m";
-Text_yellow   = "\033[01;31m";
-Text_green    = "\033[10;31m";
+Text_endcolor   = "\033[0m";
+Text_red        = "\033[22;31m";
+Text_yellow     = "\033[01;31m";
+Text_green      = "\033[10;31m";
 
-USER_NAME     = "apd"
-USER_PASSWD   = "apd"
-REMOTE_HOST   = "192.168.1.21";
-REMOTE_DB_DIR = "/home/apd/dbbackup/";
+#resource host
+USER_NAME       = "apd"
+USER_PASSWD     = "apd"
+REMOTE_HOST     = "192.168.1.21";
+REMOTE_DB_DIR   = "/home/apd/dbbackup/";
 
-HOME_PATH     = ENV['home'];
-LOCAL_DB_DIR  = "/home/wuxiang/penguin/build/";
+#other destination host
+BUSER_NAME      = "apd";
+BUSER_PASSWD    = "apd";
+BREMOTE_HOST    = "192.168.3.3";
+BREMOTE_HOME    = "/home/apd/"
+BREMOTE_DB_DIR  = "/home/apd/parallel-0.1.4.5fa66bf/bin/";
+
+#local config
+HOME_PATH       = ENV['home'];
+LOCAL_DB_DIR    = "/home/wuxiang/penguin/build/";
 
 #command
-TAR_DB        = "tar xzvf ";
+TAR_DB          = "tar xzvf ";
 
 #postgres config
-PSQL_HOST     = "192.168.154.136";
-PSQL_PORT     = "5432";
-SCANNER_FILE  = "scanner_add_access";
-SCANNER_CMD   = "./#{SCANNER_FILE} --psql_addr #{PSQL_HOST}  --psql_port #{PSQL_PORT} --user_name ";
+PSQL_HOST       = "192.168.154.136";
+PSQL_PORT       = "5432";
+SCANNER_FILE    = "scanner_add_access";
+SCANNER_CMD     = "./#{SCANNER_FILE} --psql_addr #{PSQL_HOST}  --psql_port #{PSQL_PORT} --user_name ";
 
 def print_error(text)
     puts Text_red + text + Text_endcolor;
@@ -47,20 +56,30 @@ class Get_DB
     end
 
     def run
-        if run_scp;
+        unless run_scp_download;
             print_notice("get data success");
+            run_scp_upload;
         else
             print_notice("get data failed");
         end
     end
 
-    def run_scp
+    def run_scp_download
         Net::SCP.start(REMOTE_HOST, USER_NAME, :password=>USER_PASSWD) do |scp|
             #synchronous download
             #local = REMOTE_DB_DIR + @m_name;
             print_debug("+++++++++++++++starting get db from resource host+++++++++++");
             ret = scp.download! (REMOTE_DB_DIR + @m_name), LOCAL_DB_DIR 
             print_debug("+++++++++++++++congraturation: ending get db++++++++++++++++");
+        end
+    end
+
+    def run_scp_upload
+        Net::SCP.start(BREMOTE_HOST, BUSER_NAME, :password=>BUSER_PASSWD) do |scp|
+            #synchronous upload
+            print_debug("+++++++++++++++starting put db to resource host+++++++++++");
+            ret = scp.upload! (LOCAL_DB_DIR + @m_name), BREMOTE_DB_DIR  
+            print_debug("+++++++++++++++congraturation: ending put db++++++++++++++++");
         end
     end
 
@@ -79,6 +98,49 @@ class Decompose
 
         result = system("mv " + dir + "/* " + ".");
         print_error("mv db file failed") unless result;
+
+        result = system("rm -rf " + dir);
+        print_error("rm empty directory failed") unless result;
+    end
+end
+
+class Remote_decompress
+    def initialize(name)
+        @m_name = name;
+    end
+
+    def run
+        ssh = Net::SSH.start(BREMOTE_HOST, BUSER_NAME, :password=>BUSER_PASSWD) do  |ssh|
+            #ret = ssh.exec "cd #{BREMOTE_DB_DIR}";
+            #print_error("remote change fold failed") unless ret;
+
+            ret = ssh.exec "#{TAR_DB} #{BREMOTE_DB_DIR}#{@m_name}";
+            print_error("remote tar failed") unless ret;
+
+            #dir = @m_name.partition(".").first;
+            #
+            ##cmd1 = "mv " + dir + "/* " + BREMOTE_DB_DIR;
+            #ret = ssh.exec "mv #{BREMOTE_HOME}#{dir}/*  #{BREMOTE_DB_DIR}"
+            #print_error("mv db file failed") unless ret;
+            #
+            ##cmd2 = "rm -rf " + dir;
+            #ret = ssh.exec "rm -rf #{BREMOTE_HOME}#{dir}";
+            #print_error("rm empty directory failed") unless ret;
+        end
+    end
+
+    def change_position
+        ssh = Net::SSH.start(BREMOTE_HOST, BUSER_NAME, :password=>BUSER_PASSWD) do  |ssh|
+            dir = @m_name.partition(".").first;
+            
+            #cmd1 = "mv " + dir + "/* " + BREMOTE_DB_DIR;
+            ret = ssh.exec "mv #{BREMOTE_HOME}#{dir}/*  #{BREMOTE_DB_DIR}"
+            print_error("mv db file failed") unless ret;
+            
+            #cmd2 = "rm -rf " + dir;
+            ret = ssh.exec "rm -rf #{BREMOTE_HOME}#{dir}";
+            print_error("rm empty directory failed") unless ret;
+        end
     end
 end
 
@@ -123,6 +185,10 @@ if __FILE__ == $0
     #decompress db file
     compose = Decompose.new(ARGV[0]);
     compose.run;
+
+    rcompose = Remote_decompress.new(ARGV[0]); 
+    rcompose.run;
+    rcompose.change_position;
 
     #scanner
     scanner = Scanner.new(ARGV[1]);
