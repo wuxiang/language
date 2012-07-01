93cce50ac8af90be6f0eab38537c8e4f0dd6308e
wuxiang <wuxiangfe@qq.com>
2012-07-01 09:39:42 +0800

    merge sort implement

--- c/algorithm/merge.cpp
+++ c/algorithm/merge.cpp
@@ -35,6 +35,7 @@ void print(const std::array<int, 100>& arr)
 	std::cout << "\n";
 }
 
+//merge sort from down to up
 void merge_element(std::array<int, 100>& arr, std::size_t low, std::size_t middle, std::size_t high)
 {
     std::vector<int>  temp;
@@ -78,29 +79,84 @@ void merge_element(std::array<int, 100>& arr, std::size_t low, std::size_t middl
 void merger_pass(std::array<int, 100>& arr, std::size_t length)
 {
     std::size_t i = 0;
-    for ( ; i + 2 * length - 1 < arr.size(); i += 2 * length - 1)
+    for ( ; i + 2 * length <= arr.size(); i += 2 * length)
     {
         merge_element(arr, i, i + length -1, i + 2*length - 1);
     }
 
-    if (i + length - 1 < arr.size())
+    if (i + length < arr.size())
     {
-        merge_element(arr,i, i + length - 1, arr.size());
+        merge_element(arr,i, i + length - 1, arr.size() - 1);
     }
+	else
+	{
+		std::cout << "not change" << std::endl;
+	}
 }
 
 void merger_sort(std::array<int, 100>& arr)
 {
-    for (std::size_t i = 1; i <= arr.size(); i *= 2)
+    for (std::size_t i = 1; i < arr.size(); i *= 2)
     {
         merger_pass(arr, i);
     }
 }
 
+//merge from up to down
+void merge_element(int* arr, std::size_t low, std::size_t middle, std::size_t high)
+{
+    int*  temp = (int*)malloc((high - low + 1) * sizeof(int));
+
+    std::size_t index = 0;
+    std::size_t i = low;
+    std::size_t j = middle + 1;
+	int* start = temp;
+    for (; i <= middle && j <= high; ++index)
+    {
+        if (arr[i] <= arr[j])
+        {
+            *(start + index) = arr[i++];
+        }
+        else
+        {
+            *(start + index) = arr[j++];
+        }
+    }
+
+    while (i <= middle)
+    {
+        *(start + index++) = arr[i++];
+    }
+
+    while (j <= middle)
+    {
+        *(start + index++) = arr[j++];
+    }
+
+    for (std::size_t k = 0; k < index; ++k)
+    {
+        std::swap(*(temp + k), arr[low + k]);
+    }
+}
+
+void up_merger_sort(int* arr, int low, int high)
+{
+	if (low < high)
+	{
+		int i = (low + high) / 2;
+		up_merger_sort(arr, low, i);
+		up_merger_sort(arr, i + 1, high);
+		merge_element(arr, low, i, high);
+	}
+}
+
 int main()
 {
     std::array<int, 100> arr = number();
-    merger_sort(arr);
+    //merger_sort(arr);
+    print(arr);
+	printf("+++++++++++++++++++++++++++++++++++++++++++++++++\n");
+	up_merger_sort(&arr[0], 0, 99);
     print(arr);
     return 0;
 }
