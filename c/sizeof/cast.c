004bee794d07c9d6f7682f8ad3c7588bc26cd82f
wuxiang <wuxiangfe@qq.com>
2012-08-12 23:15:49 -0700

    cast

--- c/sizeof/cast.c
+++ c/sizeof/cast.c
@@ -4,6 +4,9 @@
 int main()
 {
     uint64_t  num = 0xfefcfbfa;
+    uint64_t tmp = num >> 8;
     printf("%x, %x\n", *(unsigned char*)(&num), *((unsigned char*)(&num) + 3));
+    printf("%x\n", num);
+    printf("%x\n", tmp);
     return 0;
 }
