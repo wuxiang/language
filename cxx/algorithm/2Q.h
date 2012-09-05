e963a154e38e8cd81f2034ce218a153a23b2dd30
wuxiang <wuxiangfe@qq.com>
2012-09-04 22:28:33 -0700

    test iterator invalid

--- cxx/algorithm/2Q.h
+++ cxx/algorithm/2Q.h
@@ -10,25 +10,26 @@
 #include <boost/unordered_map.hpp>
 
 #include <boost/thread.hpp>
+enum queue_num
+{
+    _ONCE_FLAG_,
+    _SECOND_FLAG_,
+};
+
+const std::size_t TOTAL = 1200;
 const std::size_t ONCE = 360;
 const std::size_t OLDER = 840;
 
 template<typename T, typename _TP>
 class _2Q
 {
-public:
-    _2Q()
-    {
-    }
-
-    ~_2Q()
-    {
-    }
-
-
-
 private:
+    uint64_t                    total;
+
+    typedef typename std::unordered_map<T, std::pair<int, PITE> >::iterator idx_ite;
     typedef typename std::list<_TP>::iterator* PITE;
+    typedef typename std::list<_TP>::iterator  ls_ite;
+
     boost::shared_mutex         index_mtx;
     boost::unordered_map<T, std::pair<int, PITE> > index;
 
@@ -37,6 +38,75 @@ private:
 
     boost::shared_mutex         Am_mtx;
     std::list<_TP>              Am;
+
+public:
+    _2Q(): total(0)
+    {
+    }
+
+    ~_2Q()
+    {
+    }
+
+
+    _TP* find(const T& key)
+    {
+    }
+
+private:
+    // index map handle
+    bool index_get(const T& key, std::pair<int, PITE>& pir)
+    {
+        boost::shared_mutex<boost::shared_mutex> lock(index_mtx);
+        idx it = index.find(key);
+        if (it != index.end())
+        {
+            pir = *it;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool index_insert(const T& key, std::pair<int, PITE>& pir)
+    {
+        boost::unique_lock<boost::shared_mutex> lock(index_mtx);
+        index.insert(std::make_pair(key, pir));
+        return true;
+    }
+
+    bool index_del(const T& key)
+    {
+        boost::unique_lock<boost::shared_mutex> lock(index_mtx);
+        index.erase(key);
+        return true;
+    }
+
+    // A1 handle
+    _TP once_get(ls_ite& it)
+    {
+        boost::unique_lock<boost::shared_mutex>  lock(A1_mtx);
+        _TP  value =  *it;
+        A1.erase(it);
+        return value;
+    }
+
+    PITE once_insert(const _TP& val)
+    {
+        boost::unique_lock<boost::shared_mutex>  lock(A1_mtx);
+        A1.push_front(val);
+        return (&(A1.begin()));
+    }
+
+    virtual bool get_data(const T&  key)
+    {
+        return false;
+    }
+
+    bool update(const std::pair<int, PITE>& info)
+    {
+        return true;
+    }
 };
 
 #endif //TWO_QUEUE_H_
---
+++ cxx/test/iterator_error.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+    std::vector<int> vec;   
+    std::list<int>  list;
+    for(int i=0;i<10;i++)   
+    {
+        vec.push_back(i);   
+        list.push_back(i);
+    }
+
+    std::vector<int>::iterator ite = vec.begin();
+    std::advance(ite, 5);
+
+    std::vector<int>::iterator it1 = vec.begin();
+    std::advance(it1, 2);
+    std::cout << *ite << std::endl;
+    vec.erase(it1);
+    std::cout << *ite << std::endl;
+
+    std::list<int>::iterator lt = list.begin();
+    std::advance(lt, 5);
+    std::list<int>::iterator lt1 = list.begin();
+    std::advance(lt1, 2);
+    std::list<int>::iterator lt2 = list.begin();
+    std::advance(lt2, 1);
+
+    std::cout << *lt << std::endl;
+    list.erase(lt1);
+    std::cout << *lt << std::endl;
+    list.erase(lt2);
+    std::cout << *lt << std::endl;
+
+    //std::cout<<"capacity:"<<vec.capacity()<<std::endl;//查看预分配空间大小   
+    //std::vector<int>::iterator iter =vec.begin()+2;   
+    //vec.insert(iter,100);
+    //std::cout<<"capacity:"<<vec.capacity()<<std::endl;     
+    //std::cout<<*iter<<std::endl;   
+    //for(std::vector<int>::iterator it=vec.begin();it!=vec.end();it++)   
+    //    std::cout<<*it<<std::endl;  
+    return 0;
+}
