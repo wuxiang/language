51fd77469f94d8123fe666ff4d16d5a66ef4009b
wuxiang <wuxiang@qq.com>
2013-03-25 17:15:34 +0800

    strncpy null pointer to array

---
+++ c/memory/nullCopy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	char  arr[64];
+	char* tmp = NULL;
+
+	strncpy(arr, tmp, 64);
+
+	fprintf(stderr, "program  exiting\n");
+	return 0;
+}
