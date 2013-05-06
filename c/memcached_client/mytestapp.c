91535e2854d413d94bcd1bdac50fcac33bcda923
wuxiang <wuxiang@qq.com>
2013-05-06 15:35:26 +0800

    implement memcached

---
+++ c/memcached_client/mytestapp.c
@@ -0,0 +1,532 @@
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <protocol_binary.h>
+
+int sock = -1;
+bool allow_closed_read = false;
+
+enum test_return { TEST_SKIP, TEST_PASS, TEST_FAIL };
+
+typedef enum test_return (*TEST_FUNC)(void);
+struct testcase {
+    const char *description;
+    TEST_FUNC function;
+};
+
+struct addrinfo *lookuphost(const char *hostname, in_port_t port)
+{
+    struct addrinfo *ai = 0;
+    struct addrinfo hints;
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_socktype = SOCK_STREAM;
+    char service[NI_MAXSERV];
+    int error;
+
+    (void)snprintf(service, NI_MAXSERV, "%d", port);
+    if ((error = getaddrinfo(hostname, service, &hints, &ai)) != 0) {
+       if (error != EAI_SYSTEM) {
+          fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(error));
+       } else {
+          perror("getaddrinfo()");
+       }
+    }
+
+    return ai;
+}
+
+int connect_server(const char *hostname, in_port_t port, bool nonblock)
+{
+    struct addrinfo *ai = lookuphost(hostname, port);
+    int sock = -1;
+    if (ai != NULL) {
+       if ((sock = socket(ai->ai_family, ai->ai_socktype,
+                          ai->ai_protocol)) != -1) {
+          if (connect(sock, ai->ai_addr, ai->ai_addrlen) == -1) {
+             fprintf(stderr, "Failed to connect socket: %s\n",
+                     strerror(errno));
+             close(sock);
+             sock = -1;
+          } else if (nonblock) {
+              int flags = fcntl(sock, F_GETFL, 0);
+              if (flags < 0 || fcntl(sock, F_SETFL, flags | O_NONBLOCK) < 0) {
+                  fprintf(stderr, "Failed to enable nonblocking mode: %s\n",
+                          strerror(errno));
+                  close(sock);
+                  sock = -1;
+              }
+          }
+       } else {
+          fprintf(stderr, "Failed to create socket: %s\n", strerror(errno));
+       }
+
+       freeaddrinfo(ai);
+    }
+    return sock;
+}
+
+off_t raw_command(char* buf,
+                         size_t bufsz,
+                         uint8_t cmd,
+                         const void* key,
+                         size_t keylen,
+                         const void* dta,
+                         size_t dtalen) {
+    /* all of the storage commands use the same command layout */
+    protocol_binary_request_no_extras *request = (protocol_binary_request_no_extras *)buf;
+    assert(bufsz > sizeof(*request) + keylen + dtalen);
+
+    memset(request, 0, sizeof(*request));
+    request->message.header.request.magic = PROTOCOL_BINARY_REQ;
+    request->message.header.request.opcode = cmd;
+    request->message.header.request.keylen = htons(keylen);
+    request->message.header.request.bodylen = htonl(keylen + dtalen);
+    request->message.header.request.opaque = 0xdeadbeef;
+
+    off_t key_offset = sizeof(protocol_binary_request_no_extras);
+
+    if (key != NULL) {
+        memcpy(buf + key_offset, key, keylen);
+    }
+    if (dta != NULL) {
+        memcpy(buf + key_offset + keylen, dta, dtalen);
+    }
+
+    return sizeof(*request) + keylen + dtalen;
+}
+
+void validate_response_header(protocol_binary_response_no_extras *response,
+                                     uint8_t cmd, uint16_t status)
+{
+    assert(response->message.header.response.magic == PROTOCOL_BINARY_RES);
+    assert(response->message.header.response.opcode == cmd);
+    assert(response->message.header.response.datatype == PROTOCOL_BINARY_RAW_BYTES);
+    assert(response->message.header.response.status == status);
+    assert(response->message.header.response.opaque == 0xdeadbeef);
+
+    if (status == PROTOCOL_BINARY_RESPONSE_SUCCESS) {
+        switch (cmd) {
+        case PROTOCOL_BINARY_CMD_ADDQ:
+        case PROTOCOL_BINARY_CMD_APPENDQ:
+        case PROTOCOL_BINARY_CMD_DECREMENTQ:
+        case PROTOCOL_BINARY_CMD_DELETEQ:
+        case PROTOCOL_BINARY_CMD_FLUSHQ:
+        case PROTOCOL_BINARY_CMD_INCREMENTQ:
+        case PROTOCOL_BINARY_CMD_PREPENDQ:
+        case PROTOCOL_BINARY_CMD_QUITQ:
+        case PROTOCOL_BINARY_CMD_REPLACEQ:
+        case PROTOCOL_BINARY_CMD_SETQ:
+            assert("Quiet command shouldn't return on success" == NULL);
+        default:
+            break;
+        }
+
+        switch (cmd) {
+        case PROTOCOL_BINARY_CMD_ADD:
+        case PROTOCOL_BINARY_CMD_REPLACE:
+        case PROTOCOL_BINARY_CMD_SET:
+        case PROTOCOL_BINARY_CMD_APPEND:
+        case PROTOCOL_BINARY_CMD_PREPEND:
+            assert(response->message.header.response.keylen == 0);
+            assert(response->message.header.response.extlen == 0);
+            assert(response->message.header.response.bodylen == 0);
+            assert(response->message.header.response.cas != 0);
+            break;
+        case PROTOCOL_BINARY_CMD_FLUSH:
+        case PROTOCOL_BINARY_CMD_NOOP:
+        case PROTOCOL_BINARY_CMD_QUIT:
+        case PROTOCOL_BINARY_CMD_DELETE:
+            assert(response->message.header.response.keylen == 0);
+            assert(response->message.header.response.extlen == 0);
+            assert(response->message.header.response.bodylen == 0);
+            assert(response->message.header.response.cas == 0);
+            break;
+
+        case PROTOCOL_BINARY_CMD_DECREMENT:
+        case PROTOCOL_BINARY_CMD_INCREMENT:
+            assert(response->message.header.response.keylen == 0);
+            assert(response->message.header.response.extlen == 0);
+            assert(response->message.header.response.bodylen == 8);
+            assert(response->message.header.response.cas != 0);
+            break;
+
+        case PROTOCOL_BINARY_CMD_STAT:
+            assert(response->message.header.response.extlen == 0);
+            /* key and value exists in all packets except in the terminating */
+            assert(response->message.header.response.cas == 0);
+            break;
+
+        case PROTOCOL_BINARY_CMD_VERSION:
+            assert(response->message.header.response.keylen == 0);
+            assert(response->message.header.response.extlen == 0);
+            assert(response->message.header.response.bodylen != 0);
+            assert(response->message.header.response.cas == 0);
+            break;
+
+        case PROTOCOL_BINARY_CMD_GET:
+        case PROTOCOL_BINARY_CMD_GETQ:
+            assert(response->message.header.response.keylen == 0);
+            assert(response->message.header.response.extlen == 4);
+            assert(response->message.header.response.cas != 0);
+            break;
+
+        case PROTOCOL_BINARY_CMD_GETK:
+        case PROTOCOL_BINARY_CMD_GETKQ:
+            assert(response->message.header.response.keylen != 0);
+            assert(response->message.header.response.extlen == 4);
+            assert(response->message.header.response.cas != 0);
+            break;
+
+        default:
+            /* Undefined command code */
+            break;
+        }
+    } else {
+        assert(response->message.header.response.cas == 0);
+        assert(response->message.header.response.extlen == 0);
+        if (cmd != PROTOCOL_BINARY_CMD_GETK &&
+            cmd != PROTOCOL_BINARY_CMD_GATK) {
+            assert(response->message.header.response.keylen == 0);
+        }
+    }
+}
+
+static bool safe_recv(void *buf, size_t len) {
+    if (len == 0) {
+        return true;
+    }
+    off_t offset = 0;
+    do {
+        ssize_t nr = read(sock, ((char*)buf) + offset, len - offset);
+        if (nr == -1) {
+            if (errno != EINTR) {
+                fprintf(stderr, "%d Failed to read: %s\n", __LINE__, strerror(errno));
+				sleep(1);
+                //abort();
+            }
+        } else {
+            if (nr == 0 && allow_closed_read) {
+                return false;
+            }
+            assert(nr != 0);
+            offset += nr;
+        }
+    } while (offset < len);
+
+    return true;
+}
+
+bool safe_recv_packet(void *buf, size_t size) {
+    protocol_binary_response_no_extras *response = (protocol_binary_response_no_extras *)buf;
+    assert(size > sizeof(*response));
+    if (!safe_recv(response, sizeof(*response))) {
+        return false;
+    }
+    response->message.header.response.keylen = ntohs(response->message.header.response.keylen);
+    response->message.header.response.status = ntohs(response->message.header.response.status);
+    response->message.header.response.bodylen = ntohl(response->message.header.response.bodylen);
+
+    size_t len = sizeof(*response);
+
+    char *ptr = (char*)buf;
+    ptr += len;
+    if (!safe_recv(ptr, response->message.header.response.bodylen)) {
+        return false;
+    }
+
+#ifdef MESSAGE_DEBUG
+    usleep(500);
+    ptr = buf;
+    len += response->message.header.response.bodylen;
+    uint8_t val = *ptr;
+    assert(val == (uint8_t)0x81);
+    fprintf(stderr, "Received %lu bytes:", (unsigned long)len);
+    for (int ii = 0; ii < len; ++ii) {
+        if (ii % 4 == 0) {
+            fprintf(stderr, "\n   ");
+        }
+        val = *(ptr + ii);
+        fprintf(stderr, " 0x%02x", val);
+    }
+    fprintf(stderr, "\n");
+#endif
+    return true;
+}
+
+void safe_send(const void* buf, size_t len, bool hickup)
+{
+    off_t offset = 0;
+    const char* ptr = (char*)buf;
+#ifdef MESSAGE_DEBUG
+    uint8_t val = *ptr;
+    assert(val == (uint8_t)0x80);
+    fprintf(stderr, "About to send %lu bytes:", (unsigned long)len);
+    for (int ii = 0; ii < len; ++ii) {
+        if (ii % 4 == 0) {
+            fprintf(stderr, "\n   ");
+        }
+        val = *(ptr + ii);
+        fprintf(stderr, " 0x%02x", val);
+    }
+    fprintf(stderr, "\n");
+    usleep(500);
+#endif
+
+    do {
+        size_t num_bytes = len - offset;
+        if (hickup) {
+            if (num_bytes > 1024) {
+                num_bytes = (rand() % 1023) + 1;
+            }
+        }
+
+        ssize_t nw = write(sock, ptr + offset, num_bytes);
+        if (nw == -1) {
+            if (errno != EINTR) {
+                fprintf(stderr, "%d Failed to write: %s\n", __LINE__, strerror(errno));
+				sleep(1);
+                //abort();
+            }
+        } else {
+            if (hickup) {
+                usleep(100);
+            }
+            offset += nw;
+        }
+    } while (offset < len);
+}
+
+enum test_return test_binary_noop(void) {
+    union {
+        protocol_binary_request_no_extras request;
+        protocol_binary_response_no_extras response;
+        char bytes[1024];
+    } buffer;
+
+    size_t len = raw_command(buffer.bytes, sizeof(buffer.bytes),
+                             PROTOCOL_BINARY_CMD_NOOP,
+                             NULL, 0, NULL, 0);
+
+    safe_send(buffer.bytes, len, false);
+    safe_recv_packet(buffer.bytes, sizeof(buffer.bytes));
+    validate_response_header(&buffer.response, PROTOCOL_BINARY_CMD_NOOP,
+                             PROTOCOL_BINARY_RESPONSE_SUCCESS);
+
+    return TEST_PASS;
+}
+
+off_t storage_command(char*buf,
+                             size_t bufsz,
+                             uint8_t cmd,
+                             const void* key,
+                             size_t keylen,
+                             const void* dta,
+                             size_t dtalen,
+                             uint32_t flags,
+                             uint32_t exp) {
+    /* all of the storage commands use the same command layout */
+    protocol_binary_request_set *request = (protocol_binary_request_set*)buf;
+    assert(bufsz > sizeof(*request) + keylen + dtalen + 8);
+
+    memset(request, 0, sizeof(*request));
+    request->message.header.request.magic = PROTOCOL_BINARY_REQ;
+    request->message.header.request.opcode = cmd;
+    request->message.header.request.keylen = htons(keylen);
+    request->message.header.request.extlen = 8;
+    request->message.header.request.bodylen = htonl(keylen + 8 + dtalen);
+    request->message.header.request.opaque = 0xdeadbeef;
+    request->message.body.flags = flags;
+    request->message.body.expiration = exp;
+
+    off_t key_offset = sizeof(protocol_binary_request_no_extras) + 8;
+
+    memcpy(buf + key_offset, key, keylen);
+    if (dta != NULL) {
+        memcpy(buf + key_offset + keylen, dta, dtalen);
+    }
+
+    return key_offset + keylen + dtalen;
+}
+
+enum test_return test_binary_set_impl(const char *key, uint8_t cmd) {
+	fprintf(stderr, "test_binary_set_impl\n");
+    union {
+        protocol_binary_request_no_extras request;
+        protocol_binary_response_no_extras response;
+        char bytes[1024];
+    } send, receive;
+	bzero(send.bytes, 1024);
+	bzero(receive.bytes,1024);
+	char* value = {"world\r\n"};
+    //uint64_t value = 0xdeadbeefdeadcafe;
+    size_t len = storage_command(send.bytes, sizeof(send.bytes), cmd,
+                                 key, strlen(key), value, strlen(value),/*sizeof(value),*/
+                                 0, 0);
+
+    /* Set should work over and over again */
+    //int ii;
+    //for (ii = 0; ii < 10; ++ii) {
+	char  byte1[1024] = {};
+	char  byte2[1024] = {};
+	int offset = sizeof(protocol_binary_request_no_extras) + send.request.message.header.request.extlen;
+	int nkeylen = ntohs(send.request.message.header.request.keylen);
+	memcpy(byte1, send.bytes + offset, nkeylen);
+	offset += nkeylen;
+
+	int nvaluelen = ntohl(send.request.message.header.request.bodylen);
+	memcpy(byte2, send.bytes +  offset, nvaluelen - 8 - nkeylen);
+
+	fprintf(stderr, "key:%s, value:%s\n", byte1, byte2);
+        safe_send(send.bytes, len, false);
+        if (cmd == PROTOCOL_BINARY_CMD_SET) {
+            safe_recv_packet(receive.bytes, sizeof(receive.bytes));
+            validate_response_header(&receive.response, cmd,
+                                     PROTOCOL_BINARY_RESPONSE_SUCCESS);
+        }
+    //}
+
+    //if (cmd == PROTOCOL_BINARY_CMD_SETQ) {
+    //    return test_binary_noop();
+    //}
+
+    //send.request.message.header.request.cas = receive.response.message.header.response.cas;
+    //safe_send(send.bytes, len, false);
+    //if (cmd == PROTOCOL_BINARY_CMD_SET) {
+    //    safe_recv_packet(receive.bytes, sizeof(receive.bytes));
+    //    validate_response_header(&receive.response, cmd,
+    //                             PROTOCOL_BINARY_RESPONSE_SUCCESS);
+    //    assert(receive.response.message.header.response.cas != send.request.message.header.request.cas);
+    //} else {
+    //    return test_binary_noop();
+    //}
+
+    return TEST_PASS;
+}
+
+static enum test_return test_binary_get_impl(const char *key, uint8_t cmd) {
+	fprintf(stderr, "test_binary_get_impl\n");
+    union {
+        protocol_binary_request_no_extras request;
+        protocol_binary_response_no_extras response;
+        char bytes[1024];
+    } send, receive;
+
+	bzero(send.bytes, 1024);
+	bzero(receive.bytes, 1024);
+    size_t len = raw_command(send.bytes, sizeof(send.bytes), cmd,
+                             key, strlen(key), NULL, 0);
+
+    safe_send(send.bytes, len, false);
+    safe_recv_packet(receive.bytes, sizeof(receive.bytes));
+    validate_response_header(&receive.response, cmd,
+                             PROTOCOL_BINARY_RESPONSE_SUCCESS);
+	fprintf(stderr, "keylen: %d, extlen: %d, datalen: %d\n",
+				receive.response.message.header.response.keylen,
+				receive.response.message.header.response.extlen,
+				receive.response.message.header.response.bodylen);
+	char* value = receive.bytes + sizeof(protocol_binary_response_no_extras) +
+				receive.response.message.header.response.keylen +
+				receive.response.message.header.response.extlen;
+	char  byte1[1024] = {};
+	memcpy(byte1, receive.bytes + sizeof(protocol_binary_response_no_extras) +
+							receive.response.message.header.response.keylen +
+							receive.response.message.header.response.extlen,
+							receive.response.message.header.response.bodylen
+							);
+
+	fprintf(stderr, "get value: %s\n", value);
+
+    //len = storage_command(send.bytes, sizeof(send.bytes),
+    //                      PROTOCOL_BINARY_CMD_ADD,
+    //                      key, strlen(key), NULL, 0,
+    //                      0, 0);
+    //safe_send(send.bytes, len, false);
+    //safe_recv_packet(receive.bytes, sizeof(receive.bytes));
+    //validate_response_header(&receive.response, PROTOCOL_BINARY_CMD_ADD,
+    //                         PROTOCOL_BINARY_RESPONSE_SUCCESS);
+
+    ///* run a little pipeline test ;-) */
+    //len = 0;
+    //int ii;
+    //for (ii = 0; ii < 10; ++ii) {
+    //    union {
+    //        protocol_binary_request_no_extras request;
+    //        char bytes[1024];
+    //    } temp;
+    //    size_t l = raw_command(temp.bytes, sizeof(temp.bytes),
+    //                           cmd, key, strlen(key), NULL, 0);
+    //    memcpy(send.bytes + len, temp.bytes, l);
+    //    len += l;
+    //}
+
+    //safe_send(send.bytes, len, false);
+    //for (ii = 0; ii < 10; ++ii) {
+    //    safe_recv_packet(receive.bytes, sizeof(receive.bytes));
+    //    validate_response_header(&receive.response, cmd,
+    //                             PROTOCOL_BINARY_RESPONSE_SUCCESS);
+    //}
+
+    return TEST_PASS;
+}
+
+enum test_return test_binary_set(void) {
+    return test_binary_set_impl("hello", PROTOCOL_BINARY_CMD_SET);
+}
+
+static enum test_return test_binary_get(void) {
+    return test_binary_get_impl("hello", PROTOCOL_BINARY_CMD_GET);
+}
+
+struct testcase testcases[] = {
+	{"test binary set", test_binary_set},
+	{"test binary get", test_binary_get},
+    { NULL, NULL }
+};
+
+int main(int argc, char* argv[])
+{
+	sock = connect_server("10.15.107.61", 11211, true);
+    int exitcode = 0;
+    int ii = 0, num_cases = 0;
+
+    for (num_cases = 0; testcases[num_cases].description; num_cases++) {
+        /* Just counting */
+    }
+
+    printf("1..%d\n", num_cases);
+
+    for (ii = 0; testcases[ii].description != NULL; ++ii) {
+        fflush(stdout);
+        enum test_return ret = testcases[ii].function();
+        if (ret == TEST_SKIP) {
+            fprintf(stdout, "ok # SKIP %d - %s\n", ii + 1, testcases[ii].description);
+        } else if (ret == TEST_PASS) {
+            fprintf(stdout, "ok %d - %s\n", ii + 1, testcases[ii].description);
+        } else {
+            fprintf(stdout, "not ok %d - %s\n", ii + 1, testcases[ii].description);
+            exitcode = 1;
+        }
+        fflush(stdout);
+    }
+
+    return exitcode;
+}
