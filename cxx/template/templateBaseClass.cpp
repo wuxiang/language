bb8e40e6693269340721f770e033e35f594d915f
wuxiang <wuxiang@qq.com>
2013-08-09 10:28:35 +0800

    test for template class virtual

---
+++ cxx/template/templateBaseClass.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+template<typename T>
+class base
+{
+	public:
+		base(const T& v): a(v)
+		{
+		}
+
+		virtual ~base()
+		{
+		}
+
+		virtual T getValue()
+		{
+			return a;
+		}
+
+		virtual T value()
+		{
+			return a;
+		}
+
+	private:
+		T a;
+};
+
+class driver: public base<int>
+{
+	public:
+		driver(const int a): base<int>(a), my(a)
+		{
+		}
+
+		virtual int getValue()
+		{
+			return my * 10;
+		}
+
+	private:
+		int my;
+};
+
+template<typename T>
+class dr: public base<T>
+{
+	public:
+		dr(const T& v): base<T>(v), mem(v)
+		{
+		}
+
+		virtual T getValue()
+		{
+			return mem * 10;
+		}
+
+	private:
+		T   mem;
+};
+
+int main()
+{
+	dr<int>  a(8);
+	base<int>&  ref =  a;
+	std::cout << ref.value() << std::endl;
+	std::cout << ref.getValue() << std::endl;
+	return 0;
+}
